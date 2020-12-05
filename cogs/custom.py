b37fb0a09b8625a550f311771cdbf12b13ce41de
Vein05 <72024472+Vein05@users.noreply.github.com>
2020-12-05 17:15:50 +0545

    :D

--- cogs/custom.py
+++ cogs/custom.py
@@ -13,7 +13,8 @@ class vein6(commands.Cog, name= "custom"):
     @commands.Cog.listener()
     @commands.cooldown(1, 15, commands.BucketType.user)
     async def on_message(self, message):
-
+            if message.guild.id != 757098499836739594:
+                return
             mongo_url= "mongodb://Abode:vein6969@abode-shard-00-00.hkghi.mongodb.net:27017,abode-shard-00-01.hkghi.mongodb.net:27017,abode-shard-00-02.hkghi.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-l4ozdp-shard-0&authSource=admin&retryWrites=true&w=majority"
             cluster= MongoClient(mongo_url)
             db = cluster['AbodeDB']
