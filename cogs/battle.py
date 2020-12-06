61df3e3023b33e90b8ca459ed7b9da521fa799db
Vein05 <72024472+Vein05@users.noreply.github.com>
2020-12-06 15:37:13 +0545

    :D

--- cogs/battle.py
+++ cogs/battle.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
+import pymongo
+from pymongo import MongoClient
 
 color = 0xa100f2
 guild = 757098499836739594
 battle = ("put battle chnl id here")
 
+mongo_url= "mongodb://Abode:vein6969@abode-shard-00-00.hkghi.mongodb.net:27017,abode-shard-00-01.hkghi.mongodb.net:27017,abode-shard-00-02.hkghi.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-l4ozdp-shard-0&authSource=admin&retryWrites=true&w=majority"
+cluster= MongoClient(mongo_url)
 class vein11(commands.Cog, name='battle'):
     def __init__(self, client):
         self.client= client
@@ -13,7 +17,7 @@ class vein11(commands.Cog, name='battle'):
     @commands.guild_only()
     async def challenge(self, ctx, member: discord.Member):
         if ctx.guild.id != (guild):
-            return
+            return await ctx.send('Main server only ;)')
         if ctx.channel.id != (757941959796195484):
             return
         if member.id == ctx.author.id:
@@ -22,7 +26,63 @@ class vein11(commands.Cog, name='battle'):
             return
         if member.bot:
             return
-        print('ok')
+        author_id = str(ctx.message.author.id)
+        member_id = str(member.id)
+
+        db = cluster['AbodeDB']
+        collection= db['Levels']
+        hm1 = {"_id" : author_id}
+        hm2 = {"_id" : member_id}
+        author_db = collection.find(hm1)
+        member_db = collection.find(hm2)
+        if (collection.find_one({"_id": member_id})== None):
+            await ctx.send(f'{member.name} is not on the database.')
+        def check(reaction,user):
+                return user.id == member.id and user.id != 759784064361299989
+
+
+        msg = await ctx.send(f"{member.mention} {ctx.author.mention} wants to challenge you to a duel.React with <:check:773959361953267742> to accept the battle or react with <:xmark:773959363379462184> to deny the challenge.")
+        await msg.add_reaction('<:check:773959361953267742>')
+        await msg.add_reaction('<:xmark:773959363379462184>')
+
+
+        reaction, user= await self.client.wait_for("reaction_add",timeout=30, check=check)
+
+
+
+        if str(reaction.emoji) == '<:xmark:773959363379462184>':
+            return await ctx.send(f'{member.name} rejected the battle.')
+        elif str(reaction.emoji) == '<:check:773959361953267742>':
+            await msg.delete()
+            await ctx.send(f'Duel accepted.')
+
+        for lvl1 in author_db:
+                stre = lvl1['Strength']
+                sped = lvl1['Speed']
+                defen = lvl1['Defense']
+                sol = lvl1['Soul']
+                health = lvl1['Health']
+                luk = lvl1['Luck']
+                qi = lvl1['Qi']
+                pth= lvl1['Path']
+                nme = lvl1['Name']
+        for lvl in member_db:
+                stre1 = lvl['Strength']
+                sped2 = lvl['Speed']
+                defen3 = lvl['Defense']
+                sol4 = lvl['Soul']
+                health5 = lvl['Health']
+                luk6 = lvl['Luck']
+                qi7 = lvl['Qi']
+                pth8= lvl['Path']
+                nme9= lvl['Name']
+        luck_db = luk * .5
+        await ctx.send(luck_db)
+
+
+
+
+
 
 
 
