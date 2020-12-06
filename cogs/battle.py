d79ec2c82755c50583e2d116424c6007db989c20
Vein05 <72024472+Vein05@users.noreply.github.com>
2020-12-06 14:53:29 +0545

    :D

--- cogs/battle.py
+++ cogs/battle.py
@@ -1,16 +1,28 @@
 import discord
 from discord.ext import commands
 
-
+color = 0xa100f2
+guild = 757098499836739594
+battle = ("put battle chnl id here")
 
 class vein11(commands.Cog, name='battle'):
     def __init__(self, client):
         self.client= client
 
-    @commands.command()
+    @commands.command(aliases=['battle'])
     @commands.guild_only()
-    async def battle(self, ctx, member: discord.Member):
-        print("check")
+    async def challenge(self, ctx, member: discord.Member):
+        if ctx.guild.id != (guild):
+            return
+        if ctx.channel.id != (757941959796195484):
+            return
+        if member.id == ctx.author.id:
+            msg = ctx.message
+            await msg.add_reaction('<:WeirdChamp:757112297096216627>')
+            return
+        if member.bot:
+            return
+        print('ok')
 
 
 
