017be1a4045fa8992d1a4627e1eb1510c54f33de
Vein05 <man359905@gmail.com>
2021-01-16 22:47:58 +0545

    added a cool update command which will update your stats to the current standards

--- misc/update.py
+++ misc/update.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import pymongo
 from pymongo import MongoClient
+import random
+from random import randint
 
 
 
@@ -16,17 +18,20 @@ class update(commands.Cog, name="update"):
 
 	@commands.command()
 	@commands.guild_only()
+	# @commands.cooldown(1, 3600, commands.BucketType.user)
 	async def update(self, ctx):
-		author_id= (ctx.author.id)
+		author_id= str(ctx.author.id)
 		db = self.Bot.cluster1['AbodeDB']
 		collection= db['Levels']
-
 		query = {"_id": author_id}
 		find = collection.find(query)
 		
-
+        # checks if user is in the db or not
 		for lvl in find:
 			idd = lvl['_id']
+			pathDB= lvl['Path']
+			realm = lvl['Realm']
+
 			points = lvl['points']
 			medal = lvl['Leauge']
 			dao = lvl['Daos']
@@ -40,12 +45,51 @@ class update(commands.Cog, name="update"):
 			realm = lvl['Realm']
 			speci = lvl['Species']
 			pth = lvl['Path']
-			nme = lvl['Name']	
+			name = lvl['Name']
+			try:
+				wisdomDB = lvl['wisdom']
+			except:
+				wisdomDB = random.randint(50, 100)  
+			
 
 
-		#when adding new things to the levelng add em trough here.
-		await ctx.send("i don't have anything to do here tbh.")
 
+			leauge = medal
+			Qi = qi
+			Realm = realm
+			pointsO = points
+			daos = dao
+			path = pathDB
+			race = speci
+			strength = stre 
+			speed = sped
+			defense = defen
+			soul = sol
+			luck = luk
+			Hp = health 
+			print("chek1")
+			
+				
+
+			query = {"_id": str(author_id)}
+			collection.delete_one(query)
+			print("check2")
+
+			if (collection.find_one({"_id": author_id}) == None):
+				print("chc3")
+
+				user_data = {"_id": author_id, "points": pointsO, "Leauge": leauge, "Qi": Qi, "Daos": daos, "Path": path, "Realm": Realm, "Luck": luck,
+								"Species": race, "Strength": strength, "Speed": speed, "Defense": defense, "Soul": soul, "Health": Hp, "Name": name
+								, "Wisdom": wisdomDB
+								}
+				print(user_data)
+				collection.insert_one(user_data)
+				await ctx.send("Your stats have been updated, ``.stats`` to check.")
+
+
+
+		
+		
 
 
 
